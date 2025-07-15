a8e04aa5ef40ad63449cf978dfc55ce4a09f314b
Jacob Arcangeli <jarcangeli92@gmail.com>
2025-07-15 13:52:44 +0100

    WIP Character equip management

--- characters/Character.gd
+++ characters/Character.gd
@@ -1,19 +1,16 @@
-extends Node
+extends ItemContainer
 class_name Character
 
 @export var character_name : String
 
 @export var level : int = 1
 
+var equip_slots : Dictionary
+
 var debt : int = 0
 
-func get_items():
-	var items = []
-	for node in get_children():
-		if node is Item:
-			items.append(node)
-	return items
-
+func _ready():
+	equip_best_gear() #TODO: Remove from _ready
 
 func get_loaned_items():
 	var items = []
@@ -21,3 +18,18 @@ func get_loaned_items():
 		if node is Item and node.loaned_character == self:
 			items.append(node)
 	return items
+
+func equip_best_gear():
+	var items = get_items()
+	for item : Item in items:
+		if not equip_slots.has(item.primary_slot_type):
+			equip_slots[item.primary_slot_type] = item
+		else:
+			if equip_slots.get(item.primary_slot_type).get_value() < item.get_value():
+				equip_slots[item.primary_slot_type] = item
+
+func get_equipped_item(slot : Item.Slot):
+	var item = equip_slots.get(slot)
+	if item == null or item.get_parent() != self:
+		return null
+	return item

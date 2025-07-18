57b18be3a2c01f3d2bb250e681345b8ef90389c0
Jacob Arcangeli <jarcangeli92@gmail.com>
2025-07-18 16:36:09 +0100

    Prototyping POI and Encounter design

---
+++ map/EncounterData.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name EncounterData
+
+@export var encounter_name : String
+
+@export var description : String
+
+@export var monster_names : Array[String]
+
+@export var item_names : Array[String]
---
+++ map/POIData.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name POIData
+
+@export var poi_name : String
+
+@export var description : String
+
+@export var encounters : Array[EncounterData]

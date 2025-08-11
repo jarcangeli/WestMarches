5959497e6f3b1ea42833bfbda9cf89722bc06217
Jacob Arcangeli <jarcangeli92@gmail.com>
2025-08-11 20:56:40 +0100

    Buff the balancing scene

--- test/TestEncounterBalance.gd
+++ test/TestEncounterBalance.gd
@@ -1,66 +1,28 @@
 extends Control
 
-@export var party : AdventuringParty
-@export var results_text : TextEdit
+@export var parties : Node
+@export var results_container : TabContainer
 
-@onready var party_summary_ui: PartySummaryUI = %PartySummaryUI
+@onready var sheet_scene = preload("res://test/TestPartyBalanceSheet.tscn")
 
 const iterations = 100
 
-func test_encounters():
-	# Now run a sim
+func _on_button_pressed() -> void:
+	for node in results_container.get_children():
+		if node is TestPartyBalanceSheet:
+			node.queue_free()
+	
+	# Run  sims
 	var start = Time.get_ticks_msec()
-	var total_iterations := 0
 	
-	var results = {}
-	for poi : POIData in POIDatabase.pois_by_name.values():
-		var poi_results = {}
-		for encounter : EncounterData in poi.encounters:
-			# Load monsters
-			var monsters : Array[Character] = []
-			for monster_name in encounter.monster_names:
-				var monster_data : CharacterData = MonsterDatabase.get_data_by_name(monster_name)
-				if not monster_data.valid():
-					push_error("Invalid monster % in encounter %s - %s " % [monster_name, poi.poi_name, encounter.encounter_name] )
-					continue
-				var monster = Character.new(monster_data)
-				monsters.append(monster)
-			
-			# Sim combat
-			var result := CombatSim.simulate(party.get_characters(), monsters, iterations)
-			total_iterations += iterations
-			poi_results[encounter.encounter_name] = result
-		results[poi.poi_name] = poi_results
-	
-	display(results)
+	var i := 0
+	for party : AdventuringParty in parties.get_children():
+		i += 1
+		var sheet = sheet_scene.instantiate()
+		results_container.add_child(sheet)
+		sheet.test_encounters(party, iterations)
+		results_container.set_tab_title(i, party.display_name)
+
 	
 	var end = Time.get_ticks_msec()
-	print("Simulation completed in %d msecs" % (end - start))
-	print("\tor %.2f msecs per iteration" % ((end - start)/float(total_iterations)))
-	print("\tor %d usecs per iteration" % int((end - start)/float(total_iterations)*1000.))
-
-func display(results : Dictionary):
-	party_summary_ui.set_party(party)
-	
-	var texts = []
-	for poi_name in results.keys():
-		texts.append(poi_name)
-		var poi : POIData = POIDatabase.pois_by_name[poi_name]
-		for encounter_data in poi.encounters:
-			var monsters = ', '.join(encounter_data.monster_names)
-			
-			var monsters_power_level = 0
-			for monster_name in encounter_data.monster_names:
-				var monster_data : CharacterData = MonsterDatabase.get_data_by_name(monster_name)
-				var monster = Character.new(monster_data)
-				monsters_power_level += monster.get_power_level()
-			
-			texts.append("\t%s - (%d) [%s]"% [encounter_data.encounter_name, monsters_power_level, monsters])
-			
-			var result : CombatSimResults = results[poi_name][encounter_data.encounter_name]
-			texts.append("\t\t%.1f%%\t\t\t%.1f" % [ result.get_win_percentage(), result.get_average_character_deaths()])
-	
-	results_text.text = '\n'.join(texts)
-
-func _on_button_pressed() -> void:
-	test_encounters()
+	print("Total simulations completed in %d msecs" % (end - start))
---
+++ test/TestPartyBalanceSheet.gd
@@ -0,0 +1,77 @@
+extends HBoxContainer
+class_name TestPartyBalanceSheet
+
+@export var results_container : Container
+
+@onready var party_summary_ui: PartySummaryUI = %PartySummaryUI
+
+func test_encounters(party, iterations):
+	for character in party.get_characters():
+		character.equip_best_gear()
+	
+	# Now run a sim
+	var start = Time.get_ticks_msec()
+	var total_iterations := 0
+	
+	var results = {}
+	for poi : POIData in POIDatabase.pois_by_name.values():
+		var poi_results = {}
+		for encounter : EncounterData in poi.encounters:
+			# Load monsters
+			var monsters : Array[Character] = []
+			for monster_name in encounter.monster_names:
+				var monster_data : CharacterData = MonsterDatabase.get_data_by_name(monster_name)
+				if not monster_data.valid():
+					push_error("Invalid monster % in encounter %s - %s " % [monster_name, poi.poi_name, encounter.encounter_name] )
+					continue
+				var monster = Character.new(monster_data)
+				monsters.append(monster)
+			
+			# Sim combat
+			var result := CombatSim.simulate(party.get_characters(), monsters, iterations)
+			total_iterations += iterations
+			poi_results[encounter.encounter_name] = result
+		results[poi.poi_name] = poi_results
+	
+	display(party, results)
+	
+	var end = Time.get_ticks_msec()
+	print("Simulation completed in %d msecs" % (end - start))
+	print("\tor %.2f msecs per iteration" % ((end - start)/float(total_iterations)))
+	print("\tor %d usecs per iteration" % int((end - start)/float(total_iterations)*1000.))
+
+func display(party, results : Dictionary):
+	for node in results_container.get_children():
+		node.queue_free()
+	
+	party_summary_ui.set_party(party)
+	
+	var texts = []
+	for poi_name in results.keys():
+		texts.append(poi_name)
+		var poi : POIData = POIDatabase.pois_by_name[poi_name]
+		display_label_text(poi_name)
+		for _i in range(0, 4):
+			display_label_text('')
+		
+		for encounter_data in poi.encounters:
+			display_label_text(encounter_data.encounter_name)
+			
+			var monsters = ', '.join(encounter_data.monster_names)
+			
+			var monsters_power_level = 0
+			for monster_name in encounter_data.monster_names:
+				var monster_data : CharacterData = MonsterDatabase.get_data_by_name(monster_name)
+				var monster = Character.new(monster_data)
+				monsters_power_level += monster.get_power_level()
+			display_label_text(monsters_power_level)
+			var result : CombatSimResults = results[poi_name][encounter_data.encounter_name]
+			display_label_text(str(int(result.get_win_percentage())) + '%')
+			display_label_text(result.get_average_character_deaths())
+			display_label_text(monsters)
+
+func display_label_text(text):
+	var label = Label.new()
+	label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	label.text = str(text)
+	results_container.add_child(label)

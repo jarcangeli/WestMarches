c587dde155f591861cda7e818f38cdf2fb857ebf
Jacob Arcangeli <jarcangeli92@gmail.com>
2025-08-27 21:55:16 +0100

    Empty tooltip bug

--- items/ui/ItemTooltip.gd
+++ items/ui/ItemTooltip.gd
@@ -7,12 +7,12 @@ func set_item(item : Item):
 	var item_name = ""
 	var item_detail = ""
 	stats_display.set_stats([])
-	if item and is_instance_valid(item):
+	if is_instance_valid(item):
 		item_name = item.item_name + (" (loaned)" if item.loaned_character else "")
 		item_detail = item.description
 		stats_display.set_stats(item.stats.values)
+		var style_box = get_theme_stylebox("panel")
+		style_box.set('bg_color', Globals.rarity_colours[item.rarity])
 	%ItemNameLabel.text = item_name
 	%DetailLabel.text = item_detail
 	
-	var style_box = get_theme_stylebox("panel")
-	style_box.set('bg_color', Globals.rarity_colours[item.rarity])

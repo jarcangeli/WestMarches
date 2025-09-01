a91f23365675bfd7cd9c8ac04f06e019489c1ce9
Jacob Arcangeli <jarcangeli92@gmail.com>
2025-09-01 21:40:45 +0100

    Popup manager improvements

--- PopupManager.gd
+++ PopupManager.gd
@@ -1,4 +1,4 @@
-extends Control
+extends VBoxContainer
 
 @export var player_inventory : PlayerInventory
 @export var item_popup_scene : PackedScene
@@ -12,6 +12,10 @@ extends Control
 
 @onready var label: Label = $Label
 
+const MAX_POPUPS := 3
+var popups_queue = []
+const POPUP_QUEUE_TIMER := 1.0
+
 func _ready():
 	child_entered_tree.connect(on_children_changed, CONNECT_DEFERRED)
 	child_exiting_tree.connect(on_children_changed, CONNECT_DEFERRED)
@@ -22,14 +26,44 @@ func _ready():
 	gold_popup.visible = false
 	gold_popup_amount_label.text = "0"
 	on_children_changed(null)
+	
+	var popup_queue_timer = Timer.new()
+	add_child(popup_queue_timer)
+	popup_queue_timer.wait_time = POPUP_QUEUE_TIMER
+	popup_queue_timer.timeout.connect(popup_queue_check)
+	popup_queue_timer.start()
+
+func popup_queue_check():
+	if popups_queue.is_empty() or get_current_popups().size() >= MAX_POPUPS:
+		return
+	var item = popups_queue[0]
+	popups_queue.remove_at(0)
+	on_player_item_gained(item)
+	popup_queue_check()
 
 func on_children_changed(_child):
-	label.visible = get_child_count() > 2
+	label.visible = get_child_count() > 3
 
 func on_player_item_gained(item : Item):
+	if get_current_popups().size() >= MAX_POPUPS:
+		popups_queue.append(item)
+		return
 	var popup = item_popup_scene.instantiate()
+	popup.popup_closed.connect(on_popup_force_closed)
 	add_child(popup)
 	popup.set_item(item)
+
+func get_current_popups():
+	var popups = []
+	for node in get_children():
+		if node is ItemPopup:
+			popups.append(node)
+	return popups
+
+func on_popup_force_closed():
+	popups_queue.clear()
+	for node in get_current_popups():
+		node.queue_free()
 
 func on_player_gold_gained(amount : int):
 	var current_amount = int(gold_popup_amount_label.text)
--- items/ui/ItemPopup.gd
+++ items/ui/ItemPopup.gd
@@ -1,11 +1,18 @@
 extends VBoxContainer
+class_name ItemPopup
+
+signal popup_closed()
 
 @onready var item_icon: ItemIcon = %ItemIconDisabled
 @onready var despawn_timer: Timer = %DespawnTimer
 @onready var item_label: Label = %ItemLabel
 
 func _ready():
-	item_icon.item_selected.connect(on_despawn_timer_timeout)
+	item_icon.item_selected.connect(on_item_selected)
+
+func on_item_selected():
+	popup_closed.emit()
+	on_despawn_timer_timeout()
 
 func set_item(item : Item):
 	item_icon.set_item(item)
